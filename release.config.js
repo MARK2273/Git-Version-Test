8f022f09c43b7d923843612532e54a506971b5e5
MARK2273 <mehtaayush781@gmail.com>
2025-12-29 13:57:31 +0530

    fix : issue

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ["main", "develop"],
+  branches: ["main", "develop", "test"],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
