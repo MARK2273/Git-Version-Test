8733dbd720b7385914a68adbd71691222b40be5e
MARK2273 <mehtaayush781@gmail.com>
2025-12-29 14:14:51 +0530

    build: setup multi-branch release and commitlint

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ["main", "develop"],
+  branches: ["main", { name: "development", prerelease: true }],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
